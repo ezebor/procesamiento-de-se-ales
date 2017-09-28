80869b2c923243124f9acc0533a5bd8e0db88ca5
Eze <ezequiel.borenstein@gmail.com>
2017-09-28 19:43:21 +0200

    Extiendo el CSV a 2015 + agrego la columna de canceladas con cupones + detalles en los gráficos + ratio 1 contempla totalidad, entre canceladas y no canceladas

--- Analisis descuentos.R
+++ Analisis descuentos.R
@@ -2,25 +2,28 @@ dir = "/Users/ezequiel/Desktop/procesamiento-de-se-ales/datos.csv"
 cantidad.registros = length(readLines(file(dir,open="r")))
 datos <- read.table(file(dir,open="r"), sep=",", header = TRUE)
 
-cantidad.meses = 24 # 12 meses de 2016 y 12 meses de 2017
-cantidad.ordenes.no.canceladas.con.cupones.por.mes = rep(0, cantidad.meses)
-cantidad.ordenes.no.canceladas.sin.cupones.por.mes = rep(0, cantidad.meses)
+cantidad.meses = 36 # 12 meses de 2015, 12 meses de 2016 y 12 meses de 2017
+total.ordenes.con.cupones = rep(0, cantidad.meses)
+total.ordenes.sin.cupones = rep(0, cantidad.meses)
 ratio.ordenes.con.cupones.sobre.ordenes.sin.cupones = rep(0, cantidad.meses)
 for(i in 1:cantidad.registros){
   mes.indice = datos$Mes[i]
-  if (datos$Anio[i] == 2017){
+  if (datos$Anio[i] == 2016){
     mes.indice <- mes.indice + 12 
+  }else if(datos$Anio[i] == 2017){
+    mes.indice <- mes.indice + 24 
   }
-  cantidad.ordenes.no.canceladas.con.cupones.por.mes[mes.indice] <- cantidad.ordenes.no.canceladas.con.cupones.por.mes[mes.indice] + datos$TotalNoCanceladasConCupones[i]
-  cantidad.ordenes.no.canceladas.sin.cupones.por.mes[mes.indice] <- cantidad.ordenes.no.canceladas.sin.cupones.por.mes[mes.indice] + datos$TotalNoCanceladasSinCupones[i]
+  
+  total.ordenes.sin.cupones[mes.indice] <- total.ordenes.sin.cupones[mes.indice] + datos$TotalCanceladas[i] - datos$TotalCanceladasConCupones[i] 
+  total.ordenes.con.cupones[mes.indice] <- total.ordenes.con.cupones[mes.indice] + datos$TotalNoCanceladasConCupones[i] + datos$TotalCanceladasConCupones[i]
   
   # RATIO 1: razón entre órdenes con cupones y órdenes sin cupones
-  ratio.ordenes.con.cupones.sobre.ordenes.sin.cupones[mes.indice] <- cantidad.ordenes.no.canceladas.con.cupones.por.mes[mes.indice] / cantidad.ordenes.no.canceladas.sin.cupones.por.mes[mes.indice]
+  ratio.ordenes.con.cupones.sobre.ordenes.sin.cupones[mes.indice] <- total.ordenes.con.cupones[mes.indice] / total.ordenes.sin.cupones[mes.indice]
 }
 
 op <- par(mfrow = c(1,2))
 meses = seq(from = 1, to = cantidad.meses, by = 1)
-hist(cantidad.ordenes.no.canceladas.con.cupones.por.mes, 
+hist(total.ordenes.con.cupones, 
      main="Frecuencia de descuentos", 
      xlab="Cantidades de cupones aplicados", 
      ylab="Cantidades de ocurrencias", 
@@ -36,5 +39,17 @@ plot(meses,
      col="red",
      type = 'l')
 
+# Cambio de año
+abline(v = 12, lty = 1, col = 'blue')
+abline(v = 24, lty = 1, col = 'blue')
+abline(v = 36, lty = 1, col = 'blue')
+
+# Fechas especiales
+abline(v = 5, lty = 3) # Hot sale 2015
+abline(v = 11, lty = 3) # Black friday 2015
+abline(v = 17, lty = 3) # Hot sale 2016
+abline(v = 23, lty = 3) # Black friday 2016
+abline(v = 29, lty = 3) # Hot sale 2017
+abline(v = 35, lty = 3) # Black friday 2017
 
 
