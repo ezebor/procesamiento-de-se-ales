beafdd417fe5449a0de5c26f4ee55e0e518304bb
Braian Halblaub <braianhal@gmail.com>
2017-09-28 22:19:35 -0300

    Indice de órdenes canceladas con cupones sobre ordenes canceladas totales

--- Analisis descuentos.R
+++ Analisis descuentos.R
@@ -1,14 +1,24 @@
+################ CARGA DATOS ################
+
 dir = "/home/braian/R/repo/procesamiento-de-se-ales/datos.csv"
 # dir = "/Users/ezequiel/Desktop/procesamiento-de-se-ales/datos.csv"
 csv <- file(dir,open="r")
 cantidad.registros = length(readLines(csv))
 datos <- read.table(file(dir,open="r"), sep=",", header = TRUE)
 
+################ INICIALIZACIÓN DE VECTORES ################
+
 cantidad.meses = 36 # 12 meses de 2015, 12 meses de 2016 y 12 meses de 2017
 total.ordenes.con.cupones = rep(0, cantidad.meses)
 total.ordenes.sin.cupones = rep(0, cantidad.meses)
+total.ordenes.canceladas = rep(0, cantidad.meses)
+total.ordenes.canceladas.con.cupones = rep(0, cantidad.meses)
 ratio.ordenes.con.cupones.sobre.ordenes.sin.cupones = rep(0, cantidad.meses)
 indice.ordernes.con.cupones.sobre.ordenes.totales = rep(0, cantidad.meses)
+indice.ordernes.canceladas.con.cupones.sobre.ordenes.canceladas.totales = repo(0, cantidad.meses)
+
+################ CÁLCULOS ################
+
 for(i in 1:(cantidad.registros-1)){
   mes.indice = datos$Mes[i]
   if (datos$Anio[i] == 2016){
@@ -19,12 +29,18 @@ for(i in 1:(cantidad.registros-1)){
   
   total.ordenes.sin.cupones[mes.indice] <- total.ordenes.sin.cupones[mes.indice] + datos$TotalCanceladas[i] - datos$TotalCanceladasConCupones[i] 
   total.ordenes.con.cupones[mes.indice] <- total.ordenes.con.cupones[mes.indice] + datos$TotalNoCanceladasConCupones[i] + datos$TotalCanceladasConCupones[i]
+  total.ordenes.canceladas[mes.indice] <- total.ordenes.canceladas[mes.indice] + datos$TotalCanceladas[i]
+  total.ordenes.canceladas.con.cupones[mes.indice] <- total.ordenes.canceladas.con.cupones[mes.indice] + datos$TotalCanceladasConCupones[i]
   
   # RATIO 1: razón entre órdenes con cupones y órdenes sin cupones
   ratio.ordenes.con.cupones.sobre.ordenes.sin.cupones[mes.indice] <- total.ordenes.con.cupones[mes.indice] / total.ordenes.sin.cupones[mes.indice]
 
   # ÍNDICE 1: órdenes con cupones sobre totales
-  indice.ordernes.con.cupones.sobre.ordenes.totales[mes.indice] <- 100 * (total.ordenes.con.cupones[mes.indice] / (total.ordenes.con.cupones[mes.indice] * total.ordenes.sin.cupones[mes.indice]))
+  indice.ordernes.con.cupones.sobre.ordenes.totales[mes.indice] <- 100 * (total.ordenes.con.cupones[mes.indice] / (total.ordenes.con.cupones[mes.indice] + total.ordenes.sin.cupones[mes.indice]))
+
+  # ÍNDICE 2: órdenes canceladas con cupones sobre totales canceladas
+  indice.ordernes.canceladas.con.cupones.sobre.ordenes.canceladas.totales[mes.indice] <- 100 * (total.ordenes.canceladas.con.cupones[mes.indice] / total.ordenes.canceladas[mes.indice])
+  
 }
 
 meses = seq(from = 1, to = cantidad.meses, by = 1)
@@ -38,7 +54,7 @@ plot(meses,
      ratio.ordenes.con.cupones.sobre.ordenes.sin.cupones,
      main="Órdenes con cupones vs. Órdenes sin cupones", 
      xlab="Meses", 
-     ylab="Razón con.cupones / sin.cupones",
+     ylab="Razón Con cupones / Sin cupones",
      col="red",
      type = 'l')
 
@@ -69,7 +85,7 @@ plot(meses,
      indice.ordernes.con.cupones.sobre.ordenes.totales,
      main="Órdenes con cupones sobre órdenes totales", 
      xlab="Meses", 
-     ylab="% con.cupones / (sin.cupones + con.cupones)",
+     ylab="% Con cupones / Totales",
      col="red",
      type = 'l')
 
@@ -83,5 +99,27 @@ abline(v = 17, lty = 3) # Hot sale 2016
 abline(v = 23, lty = 3) # Black friday 2016
 abline(v = 29, lty = 3) # Hot sale 2017
 abline(v = 35, lty = 3) # Black friday 2017
+
+# ÍNDICE 2
+
+plot(meses, 
+     indice.ordernes.canceladas.con.cupones.sobre.ordenes.canceladas.totales,
+     main="Órdenes canceladas con cupones sobre totales canceladas", 
+     xlab="Meses", 
+     ylab="% Con cupones canceladas / canceladas",
+     col="red",
+     type = 'l')
+
+abline(v = 12, lty = 1, col = 'blue') # Fin 2015
+abline(v = 24, lty = 1, col = 'blue') # Fin 2016
+abline(v = 36, lty = 1, col = 'blue') # Fin 2017
+
+abline(v = 5, lty = 3) # Hot sale 2015
+abline(v = 11, lty = 3) # Black friday 2015
+abline(v = 17, lty = 3) # Hot sale 2016
+abline(v = 23, lty = 3) # Black friday 2016
+abline(v = 29, lty = 3) # Hot sale 2017
+abline(v = 35, lty = 3) # Black friday 2017
+
 
 
