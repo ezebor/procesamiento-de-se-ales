4c1f6c7e3c2d0c4747e42bb051bab274b5eb70fb
Eze <ezequiel.borenstein@gmail.com>
2017-10-09 08:38:33 +0200

    Agrego gráfico de totales en $ + refactors de código

--- Analisis descuentos.R
+++ Analisis descuentos.R
@@ -16,6 +16,8 @@ total.ordenes.canceladas.con.cupones = rep(0, cantidad.meses)
 ratio.ordenes.con.cupones.sobre.ordenes.sin.cupones = rep(0, cantidad.meses)
 indice.ordernes.con.cupones.sobre.ordenes.totales = rep(0, cantidad.meses)
 indice.ordernes.canceladas.con.cupones.sobre.ordenes.canceladas.totales = rep(0, cantidad.meses)
+montos.totales = rep(0, cantidad.meses)
+montos.totales.descontado = rep(0, cantidad.meses)
 
 ################ CÁLCULOS ################
 
@@ -31,6 +33,9 @@ for(i in 1:(cantidad.registros-1)){
   total.ordenes.con.cupones[mes.indice] <- total.ordenes.con.cupones[mes.indice] + datos$TotalNoCanceladasConCupones[i] + datos$TotalCanceladasConCupones[i]
   total.ordenes.canceladas[mes.indice] <- total.ordenes.canceladas[mes.indice] + datos$TotalCanceladas[i]
   total.ordenes.canceladas.con.cupones[mes.indice] <- total.ordenes.canceladas.con.cupones[mes.indice] + datos$TotalCanceladasConCupones[i]
+  
+  montos.totales[mes.indice] <- montos.totales[mes.indice] + as.numeric(datos$Total[i])
+  montos.totales.descontado[mes.indice] <- montos.totales.descontado[mes.indice] + as.numeric(datos$TotalDiscount[i])
   
   # RATIO 1: razón entre órdenes con cupones y órdenes sin cupones
   ratio.ordenes.con.cupones.sobre.ordenes.sin.cupones[mes.indice] <- total.ordenes.con.cupones[mes.indice] / total.ordenes.sin.cupones[mes.indice]
@@ -49,37 +54,11 @@ meses <- seq(tmin, tmax, by="month")
 
 close(csv)
 
-################ GRÁFICOS ################
-
-# RATIO 1
-plot.index(ratio.ordenes.con.cupones.sobre.ordenes.sin.cupones, 
-           "Órdenes con cupones vs. Órdenes sin cupones",
-           "Razón Con cupones / Sin cupones")
-
-# Histograma de frecuencias de descuentos
-hist(total.ordenes.con.cupones, 
-     main="Frecuencia de descuentos", 
-     xlab="Cantidades de cupones aplicados", 
-     ylab="Cantidades de ocurrencias", 
-     col="green",
-     xlim=c(0,25000),
-     las=1, 
-     breaks=5)
-
-# ÍNDICE 1
-plot.index(indice.ordernes.con.cupones.sobre.ordenes.totales, 
-           "Órdenes con cupones sobre órdenes totales",
-           "% Con cupones / Totales")
-
-# ÍNDICE 2
-plot.index(indice.ordernes.canceladas.con.cupones.sobre.ordenes.canceladas.totales, 
-           "Órdenes canceladas con cupones sobre totales canceladas",
-           "% Con cupones canceladas / canceladas")
-
 ################ FUNCIONES AUXILIARES ################
 
 plot.index = function(ratio, titulo, y.label){
   lab <- format(meses,format="%Y-%b")
+  op <- par(mar = c(5,7,4,2) + 0.1)
   plot(meses, 
        ratio,
        main= titulo, 
@@ -96,6 +75,26 @@ plot.index = function(ratio, titulo, y.label){
   plot.lines()
 }
 
+plot.montos.totales = function(titulo, y.label){
+  lab <- format(meses,format="%Y-%b")
+  op <- par(mar = c(5,7,4,2) + 0.1)
+  plot(meses, 
+       montos.totales,
+       main= titulo, 
+       ylab= y.label,
+       col= "red",
+       type = 'l',
+       xaxt="n", 
+       xlab="",
+       las = 2
+  )
+  axis(1, at=meses, labels=FALSE)
+  text(x=meses, y=par()$usr[3]-0.1*(par()$usr[4]-par()$usr[3]),
+       labels=lab, srt=45, adj=0.8, xpd=TRUE)
+  plot.lines()
+  lines(meses, montos.totales.descontado, col='green')
+}
+
 plot.lines = function(){
   abline(v = meses[12], lty = 1, col = 'blue') # Fin 2015
   abline(v = meses[24], lty = 1, col = 'blue') # Fin 2016
@@ -108,3 +107,33 @@ plot.lines = function(){
   abline(v = meses[29], lty = 3) # Hot sale 2017
   abline(v = meses[35], lty = 3) # Black friday 2017
 }
+
+################ GRÁFICOS ################
+
+# RATIO 1
+plot.index(ratio.ordenes.con.cupones.sobre.ordenes.sin.cupones, 
+           "Órdenes con cupones sobre órdenes sin cupones",
+           "Razón: órdenes con cupones / órdenes sin cupones")
+
+# Histograma de frecuencias de descuentos
+hist(total.ordenes.con.cupones, 
+     main="Frecuencia de descuentos", 
+     xlab="Cantidades de cupones aplicados", 
+     ylab="Cantidades de ocurrencias", 
+     col="green",
+     xlim=c(0,25000),
+     las=1, 
+     breaks=5)
+
+# ÍNDICE 1
+plot.index(indice.ordernes.con.cupones.sobre.ordenes.totales, 
+           "Órdenes con cupones sobre órdenes totales",
+           "% Órdenes con cupones sobre órdenes totales")
+
+# ÍNDICE 2
+plot.index(indice.ordernes.canceladas.con.cupones.sobre.ordenes.canceladas.totales, 
+           "Órdenes canceladas con cupones sobre órdenes canceladas",
+           "% Órdenes canceladas con cupones sobre órdenes canceladas")
+
+# MONTOS TOTALES VS MONTOS DESCONTADOS
+plot.montos.totales("Montos totales y montos descontdos", "")
